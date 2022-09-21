4af2d2236fe79384e01a073f875cafdbd0ea427d
Hasnain Raza <Hassanain.okz110@gmail.com>
2022-09-21 12:02:28 +0500

    FIrst commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Providers/Comment.dart';
+import 'UI/NewFullScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        //ChangeNotifierProvider(create: (_) => likeColor()),
+        ChangeNotifierProvider(create: (_) => Comment())
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const NewFullScreen(),
+      ),
+    );
+  }
+}
